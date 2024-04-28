76d5ce533850b3345f0673889bcf8d15acac5f55
gasvil17 <gasvil17@gmail.com>
2024-04-28 04:08:53 +0000

    feat: initial config

---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,12 @@
+import { html } from "lit"
+import "../components/button/button"
+
+const meta = {
+    title: "Example/Button",
+    tags: ["autodocs"],
+    render: () => html`
+        <button>Button</button>
+    `
+}
+
+export default meta
